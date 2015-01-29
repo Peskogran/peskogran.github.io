7945be8180b96fab3c925bd39264743f292db8b0
SedovAnton <pymen@elance-odesk.com>
2015-01-29 16:49:24 +0300

    oborud

--- captcha/imkeys.php
+++ captcha/imkeys.php
@@ -1,4 +1,4 @@
 <?php
 	$oNameList = array("epn","4xr","ks5","4yp","fk5","tna","gd8","mxy","c7d","cjk");
-	$oCharList = array("D","T","J","W","H","F","N","E","4","M");
+	$oCharList = array("A","S","M","K","4","2","T","3","V","S");
 // End of file imkeys.php
